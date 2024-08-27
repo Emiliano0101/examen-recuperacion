efa27057651d823543142bd96c04bca17e0a666d
Tu Nombre <emilianoceron15@gmail.com>
2024-08-26 19:39:45 -0600

    exammen recu

---
+++ data.tsx
@@ -0,0 +1,257 @@
+import { BookText, CodeSquare, HomeIcon, UserRound, Linkedin, Twitter, Rss, Twitch, Youtube, Crop, Pencil, Computer, Book, Rocket, Speech } from "lucide-react";
+
+export const socialNetworks = [
+    {
+        id: 1,
+        logo: <Youtube size={30} strokeWidth={1} />,
+        src: "https://www.youtube.com/@alucard462",
+    },
+    {
+        id: 2,
+        logo: <Linkedin size={30} strokeWidth={1} />,
+        src: "#!",
+    },
+    {
+        id: 3,
+        logo: <Twitter size={30} strokeWidth={1} />,
+        src: "https://x.com/EmilianoCern2",
+    },
+    {
+        id: 4,
+        logo: <Rss size={30} strokeWidth={1} />,
+        src: "#!",
+    },
+    {
+        id: 5,
+        logo: <Twitch size={30} strokeWidth={1} />,
+        src: "https://www.twitch.tv/alucard_3810",
+    },
+];
+
+
+export const itemsNavbar = [
+    {
+        id: 1,
+        title: "Home",
+        icon: <HomeIcon size={25} color="#fff" strokeWidth={1} />,
+        link: "/",
+    },
+    {
+        id: 2,
+        title: "User",
+        icon: <UserRound size={25} color="#fff" strokeWidth={1} />,
+        link: "/about-me",
+    },
+    {
+        id: 3,
+        title: "Book",
+        icon: <BookText size={25} color="#fff" strokeWidth={1} />,
+        link: "/services",
+    },
+    {
+        id: 4,
+        title: "Target",
+        icon: <CodeSquare size={25} color="#fff" strokeWidth={1} />,
+        link: "/portfolio",
+    },
+    {
+        id: 5,
+        title: "Home",
+        icon: <Speech size={25} color="#fff" strokeWidth={1} />,
+        link: "/testimonials",
+    },
+];
+
+export const dataAboutPage = [
+    {
+        id: 1,
+        title: "Estudiante de Ingeniería en Desarrollo de Software",
+        subtitle: "Universidad Politécnica de Pachuca",
+        description: "Actualmente cursando el cuarto cuatrimestre de la carrera, adquiriendo conocimientos en programación, desarrollo de software y creación de páginas web, con un enfoque en la construcción de soluciones tecnológicas innovadoras.",
+        date: "Ago 2023",
+    },
+    {
+        id: 2,
+        title: "Desarrollador Web Junior",
+        subtitle: "Proyectos Personales",
+        description: "Creación y mantenimiento de sitios web utilizando HTML, CSS y JavaScript, aplicando los conocimientos adquiridos en la universidad para desarrollar proyectos que refuercen y amplíen mis habilidades técnicas.",
+        date: "May 2023",
+    },
+    {
+        id: 3,
+        title: "Colaborador en Proyectos Académicos",
+        subtitle: "UPP - Desarrollo de Software",
+        description: "Participación en proyectos grupales en la universidad, donde colaboro con compañeros para desarrollar aplicaciones web y móviles, resolviendo problemas reales y aplicando metodologías ágiles.",
+        date: "Feb 2023",
+    },
+    {
+        id: 4,
+        title: "Asistente en Talleres de Programación",
+        subtitle: "UPP - Talleres de Extensión",
+        description: "Asistencia en la organización y ejecución de talleres de programación para estudiantes de nuevo ingreso, ayudando a otros a adquirir habilidades fundamentales en desarrollo web y lógica de programación.",
+        date: "Nov 2022",
+    },
+];
+
+export const dataCounter = [
+    {
+        id: 0,
+        endCounter: 4,
+        text: "Cuatrimestres completados",
+        lineRight: true,
+        lineRightMobile: true,
+    },
+    {
+        id: 1,
+        endCounter: 3,
+        text: "Proyectos académicos",
+        lineRight: true,
+        lineRightMobile: false,
+    },
+    {
+        id: 2,
+        endCounter: 2,
+        text: "Lenguajes aprendidos",
+        lineRight: true,
+        lineRightMobile: true,
+    },
+    {
+        id: 3,
+        endCounter: 0,
+        text: "Reconocimientos obtenidos",
+        lineRight: false,
+        lineRightMobile: false,
+    },
+];
+
+export const serviceData = [
+    {
+        icon: <Crop />,
+        title: "Branding",
+        description: "Desarrollo de una identidad de marca sólida y coherente, incluyendo diseño de logotipo, colores y elementos visuales",
+    },
+    {
+        icon: <Pencil />,
+        title: "Diseño web",
+        description: "Diseño creativo y profesional de interfaces web intuitivas y atractivas, centradas en la experiencia del usuario",
+    },
+    {
+        icon: <Computer />,
+        title: "Desarrollo web",
+        description: "Diseño y desarrollo de sitios web a medida, adaptados a tus necesidades",
+    },
+    {
+        icon: <Book />,
+        title: "Copywriting",
+        description: "Creación de contenido persuasivo y atractivo que capta la atención de tu audiencia",
+    },
+    {
+        icon: <Rocket />,
+        title: "SEO",
+        description: "Optimización de tu presencia en línea mediante estrategias de SEO avanzadas, ",
+    },
+];
+
+export const dataPortfolio = [
+    {
+        id: 1,
+        title: "Web Pro",
+        image: "/image-1.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 2,
+        title: "Desarrollo Web Ágil",
+        image: "/image-2.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 3,
+        title: "Estrategias Web",
+        image: "/image-3.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 4,
+        title: "Ideas Creativas",
+        image: "/image-4.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 5,
+        title: "Webs Impactantes",
+        image: "/image-5.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 6,
+        title: "Web Dinámica",
+        image: "/image-6.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 7,
+        title: "Dark Web ",
+        image: "/image-7.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 8,
+        title: "E-commerce web",
+        image: "/image-8.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    }
+];
+
+export const dataTestimonials = [
+    {
+        id: 1,
+        name: "George Snow",
+        description:
+            "¡Increíble plataforma! Los testimonios aquí son genuinos y me han ayudado a tomar decisiones informadas. ¡Altamente recomendado!",
+        imageUrl: "/profile1.png",
+    },
+    {
+        id: 2,
+        name: "Juan Pérez",
+        description:
+            "Me encanta la variedad de testimonios disponibles en esta página. Es inspirador ver cómo otras personas han superado desafíos similares a los míos. ¡Gracias por esta invaluable fuente de motivación!",
+        imageUrl: "/profile2.png",
+    },
+    {
+        id: 3,
+        name: "María García",
+        description:
+            "Excelente recurso para obtener opiniones auténticas sobre diferentes productos y servicios. Me ha ayudado mucho en mis compras en línea. ¡Bravo por este sitio!",
+        imageUrl: "/profile3.png",
+    },
+    {
+        id: 4,
+        name: "Laura Snow",
+        description:
+            "¡Qué descubrimiento tan fantástico! Los testimonios aquí son honestos y detallados. Me siento más seguro al tomar decisiones después de leer las experiencias compartidas por otros usuarios.",
+        imageUrl: "/profile4.png",
+    },
+    {
+        id: 5,
+        name: "Carlos Sánchez",
+        description:
+            "Una joya en la web. Los testimonios son fáciles de encontrar y están bien organizados. ¡Definitivamente mi destino número uno cuando necesito referencias confiables!",
+        imageUrl: "/profile5.png",
+    },
+    {
+        id: 6,
+        name: "Antonio Martínez",
+        description:
+            "¡Fantástico recurso para aquellos que buscan validación antes de tomar decisiones importantes! Los testimonios aquí son veraces y realmente útiles. ¡Gracias por simplificar mi proceso de toma de decisiones!",
+        imageUrl: "/profile6.png",
+    },
+];
